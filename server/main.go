179ec9efa3a883e2dffdabb116f5ab39c235d560
Dries De Peuter <dries@nousefreak.be>
2018-01-01 02:30:09 +0100

    Open browser on start

--- server/main.go
+++ server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "os"
+import (
+	"os"
+	"fmt"
+	"github.com/skratchdot/open-golang/open"
+)
 
 func main() {
 	hub := newHub()
@@ -9,6 +13,12 @@ func main() {
 	mapInstance := LoadMap(os.Args[1])
 
 	e := CreateServer(hub, mapInstance)
+
+	fmt.Println("Starting...")
+	err := open.Run("http://127.0.0.1:9000/")
+	if err != nil {
+		fmt.Println("Open http://127.0.0.1:9000 in your favorite browser.")
+	}
 
 	e.Logger.Fatal(e.Start(":9000"))
 }
